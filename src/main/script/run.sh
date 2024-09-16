fdb3a1e791723aade9a8e410279729018cb28ef0
ivan.uroz@origen.bio <ivan.uroz@origen.bio>
2024-09-16 10:59:30 +0200

    Modificación para ejecución del contenedor

--- src/main/script/run.sh
+++ src/main/script/run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
- 
-#docker run --rm  --name=opendata-crawler  opendata-crawler:1.0.0
-
-docker run -it --name=opendata-crawler  opendata-crawler:1.0.0 /bin/bash
+ docker run --rm  --name=opendata-crawler  opendata-crawler:1.0.0
